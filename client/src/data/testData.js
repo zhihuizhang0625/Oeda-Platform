747bec0f4d98a2464b43270b5d6a4b06c08eb5d9
Joyce An-Jie Wang <joycewang821993@gmail.com>
2022-11-25 18:25:31 -0500

    Update test data

--- client/src/data/testData.js
+++ client/src/data/testData.js
@@ -5,6 +5,7 @@
     // "number_of_customer": [{ "Number": 96189, "Increment": 21.58 }],
     "number_of_5_review" : [{"Number": 5422, "Percentage": 55.11}],
     "number_of_states" : [{ "states": 27 }],
-    "sales_generated_per_year" : [{ "product_category": 'auto', "sales": 384480.07, "year": 2017 }]
+    "sales_generated_per_year" : [{ "product_category": 'auto', "sales": 384480.07, "year": 2017 }],
+    "recent_transactions": [{}]
     
 }
