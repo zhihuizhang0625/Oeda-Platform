8a22112d7ada0a6b374f091a9f78c6892700b68f
Joyce An-Jie Wang <joycewang821993@gmail.com>
2022-11-25 18:30:34 -0500

    added recent transaction data

--- client/src/data/testData.js
+++ client/src/data/testData.js
@@ -6,6 +6,6 @@
     "number_of_5_review" : [{"Number": 5422, "Percentage": 55.11}],
     "number_of_states" : [{ "states": 27 }],
     "sales_generated_per_year" : [{ "product_category": 'auto', "sales": 384480.07, "year": 2017 }],
-    "recent_transactions": [{}]
+    "recent_transactions": [{"order_id": '3b697a20d9e427646d92567910af6d57',  "date": '2016-10-03 09:44:50', "price": 45.46}]
     
 }
